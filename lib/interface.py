502cd0a6b4f998b10d188013996eb5883b1d5d09
demo <available3099@gmail.com>
2020-06-11 10:58:51 +0800

    commitlogs

--- lib/interface.py
+++ lib/interface.py
@@ -117,3 +117,19 @@ def subscriberoot(setting_id):
     encodestr = base64.b64encode(shuchu.encode('utf-8'))
     print(str(encodestr, 'utf-8'))
     return str(encodestr, 'utf-8')
+
+
+@server.route('/commitlogs', methods=['GET', 'POST'])
+def getcommitlogs():
+    file_object = open('//var//local//gitlog')
+    # 不要把open放在try中，以防止打开失败，那么就不用关闭了
+    try:
+        file_context = file_object.read()
+        # file_context是一个string，读取完后，就失去了对test.txt的文件引用
+        #  file_context = open(file).read().splitlines()
+        # file_context是一个list，每行文本内容是list中的一个元素
+    finally:
+        file_object.close()
+    # 除了以上方法，也可用with、contextlib都可以打开文件，且自动关闭文件，
+    # 以防止打开的文件对象未关闭而占用内存
+    return file_context
