13f93d0c9a73c587984a90b386f2d06dcb3b0cb4
demo <available3099@gmail.com>
2020-06-13 12:12:05 +0800

    修改

--- lib/interface.py
+++ lib/interface.py
@@ -36,14 +36,18 @@ def login():
 
 @server.route('/logout', methods=['GET', 'POST'])
 def logout():
-    url = 'https://www.google.com/search?newwindow=1&biw=1091&bih=763'
-
-    params = parse.parse_qs(parse.urlparse(url).query)
-
-    params['newwindow']
-
-    print(params['bih'])
-    return "heheheh"
+    file_object = open('/var/local/gitlog')
+    # 不要把open放在try中，以防止打开失败，那么就不用关闭了
+    try:
+        file_context = file_object.read()
+        # file_context是一个string，读取完后，就失去了对test.txt的文件引用
+        #  file_context = open(file).read().splitlines()
+        # file_context是一个list，每行文本内容是list中的一个元素
+    finally:
+        file_object.close()
+    # 除了以上方法，也可用with、contextlib都可以打开文件，且自动关闭文件，
+    # 以防止打开的文件对象未关闭而占用内存
+    return file_context
 
 
 @server.route('/netlify/<setting_id>', methods=['GET', 'POST'])
