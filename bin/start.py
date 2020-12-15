2699cbae2205c88b5bdd39e71a4fcfde88441007
demo <available3099@gmail.com>
2020-12-15 15:02:03 +0800

    修改

--- bin/start.py
+++ bin/start.py
@@ -3,7 +3,11 @@ import sys
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE_PATH)
-from lib.interface import server
 from conf.setting import SERVER_PORT
+from  lib.subscribe import sub_se
+from lib.mysql_util import conn_mysql
 
-server.run(host='0.0.0.0', port=SERVER_PORT, debug=True)
+mysqlsesson = conn_mysql()
+
+
+sub_se.run(host='0.0.0.0',port=SERVER_PORT,debug=True)
